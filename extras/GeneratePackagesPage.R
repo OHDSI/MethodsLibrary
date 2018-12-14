4a89a32344274999f7b4ff1f778b77b7890a5439
schuemie <schuemie@ohdsi.org>
2018-12-14 13:44:30 +0100

    Adding more text to the various sections.

--- extras/GeneratePackagesPage.R
+++ extras/GeneratePackagesPage.R
@@ -1,17 +1,16 @@
-packages <- read.csv("extras/packages.csv")
+packages <- read.csv("extras/packages.csv", stringsAsFactors = FALSE)
+sections <- read.csv("extras/sections.csv", stringsAsFactors = FALSE)
 
-lines <- c("---
-title: 'Packages'
-output: 
-    html_document:
-        toc: TRUE
-        toc_float: TRUE
----")
+headerFile <- "extras/packagesHeader.Rmd"
+lines <- gsub("\r", "", readChar(headerFile, file.info(headerFile)$size))
+lines <- c(lines, "")
 section <- ""
 for (i in 1:nrow(packages)) {
   if (packages$section[i] != section) {
     section <- packages$section[i]
     lines <- c(lines, paste("#", section))
+    lines <- c(lines, "")
+    lines <- c(lines, sections$description[sections$section == packages$section[i]])
     lines <- c(lines, "")
   }
   name <- packages$name[i]
