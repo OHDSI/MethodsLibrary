e868806871b4f3583af94e2007cca56d34a06589
schuemie <schuemie@ohdsi.org>
2020-04-03 12:05:29 +0200

    Updating documentation. Started adding developer guidelines.

--- extras/SiteMaintenance.R
+++ extras/SiteMaintenance.R
@@ -1,9 +1,12 @@
 # Generate site from markdown files --------------------------------------
 
+# You'll need the icon package:
+devtools::install_github("ropenscilabs/icon")
+
 # Run this in standalone R session. Runs orders of magnitude faster compared 
 # to running in RStudio:
 
-setwd("C:/Users/mschuemi/Git/MethodsLibrary")
+setwd("C:/Git/MethodsLibrary")
 rmarkdown::render_site()
 
 
