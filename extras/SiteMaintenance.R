8007ffcba9156e081d988478de605bec166b353e
schuemie <schuemie@ohdsi.org>
2020-06-15 16:49:33 +0200

    Referring to HADES site

--- extras/SiteMaintenance.R
+++ extras/SiteMaintenance.R
@@ -3,13 +3,13 @@
 # You'll need the icon package:
 devtools::install_github("ropenscilabs/icon")
 
+# Generate packages markdown file
+source("extras/GeneratePackagesPage.R")
+
+# Generate support markdown file
+source("extras/GenerateSupportPage.R")
+
 # Run this in standalone R session. Runs orders of magnitude faster compared 
 # to running in RStudio:
-
 setwd("C:/Git/MethodsLibrary")
 rmarkdown::render_site()
-
-
-# Generate packages markdown file
-
-source("extras/GeneratePackagesPage.R")
